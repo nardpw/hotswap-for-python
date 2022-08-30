a5826b1bc2d1014380b37c02d612be8e2e6876b2
230 <82015149+nardpw@users.noreply.github.com>
2022-08-30 20:30:53 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,45 @@ import datetime
 from setuptools import setup, find_packages
 from setuptools.command.install import install
 
-readme = open('./README.md').read()
+try:
+    readme = open('README.md').read()
+except:
+    readme = '''# Hotswap for Python
+
+# Install
+
+`pip install hotswap`
+
+# Usage
+
+Decorate the main function with @hotswap.main
+
+# Example
+```python
+import hotswap
+from time import sleep
+
+def function():
+    print('hoge')
+
+class Calculater:
+
+    def add(self, x, y):
+        return x + y
+
+@hotswap.main # add this to main function
+def main():   # cannot be hot-swapped to the main function
+    calc = Calculater()
+    while True:
+        function()
+        print(calc.add(2, 3))
+        sleep(1)
+
+if __name__ == '__main__':
+    main()
+
+```
+'''
 
 VERSION = '1.0.0'
 
